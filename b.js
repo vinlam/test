92d71115067564a6548882654841eb8a4832874b
vinlam <zwlvin@163.com>
2019-12-17 18:45:18 +0800

    add

--- b.js
+++ b.js
@@ -1,6 +1,15 @@
 (function($, undefined) {
 	$.mbank = $.mbank || {};
 	$.extend($.mbank, {
-		a: '123'
-	});
+		a: '123',
+		send: function(method, param, succfun, errfun, flag) {
+			//console.log(method);
+			//console.log(JSON.stringify(this));
+			//var obj = JSON.stringify(this);
+			//console.log(JSON.parse(obj).a);
+			//console.log($(this));
+			console.log(this.a);
+			console.log($.mbank.a);
+		}
+	})
 })(jQuery)
